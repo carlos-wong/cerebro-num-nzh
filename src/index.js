8894d51425f91fe4125ed217ff5f5c29b0783040
carlos <huaixian.huang@gmail.com>
2021-06-24 15:13:11 +0800

    feat: copy number shen enter on item

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 var nzhcn = require("nzh/cn");
+const { clipboard, nativeImage } = require("electron");
 
 export const fn = ({ term, display }) => {
   if (term.match('^nzh ')){
     var num = nzhcn.encodeB(term.split(" ")[1]);
     display({
-      title: `${num}`
+      title: `${num}`,
+      onSelect:()=>{
+        clipboard.writeText(`${num}`);
+      }
     })
   }
 }
